fca050e62687bd40464021e128156844c5f1d289
superrepublic <superrepublic@163.com>
2017-12-15 13:54:01 +0800

    Initial Commit

---
+++ PaygisDIDSwift/PaygisDIDSwift.h
@@ -0,0 +1,19 @@
+//
+//  PaygisDIDSwift.h
+//  PaygisDIDSwift
+//
+//  Created by superpein on 2017/12/15.
+//  Copyright © 2017年 superpein. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PaygisDIDSwift.
+FOUNDATION_EXPORT double PaygisDIDSwiftVersionNumber;
+
+//! Project version string for PaygisDIDSwift.
+FOUNDATION_EXPORT const unsigned char PaygisDIDSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PaygisDIDSwift/PublicHeader.h>
+
+
