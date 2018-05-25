e61106765628227d4deb28bfc58d16289abe6fab
superrepublic <superrepublic@163.com>
2018-05-25 10:52:03 +0800

    swift版本采集信息

---
+++ PaygisDIDSwift/PayegisDID.swift
@@ -0,0 +1,19 @@
+//
+//  PayegisDID.swift
+//  PaygisDIDSwift
+//
+//  Created by superpein on 2017/12/15.
+//  Copyright © 2017年 superpein. All rights reserved.
+//
+
+import UIKit
+import Foundation
+class PayegisDID: NSObject {
+    override init() {
+          func someFunctionThatTakesAClosure(closure: () -> Void) {
+            // function body goes here
+            print()
+        }
+    }
+    
+}
--- PaygisDIDSwift/PaygisDIDSwift.h
+++ PaygisDIDSwift/PaygisDIDSwift.h
@@ -16,4 +16,5 @@ FOUNDATION_EXPORT const unsigned char PaygisDIDSwiftVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <PaygisDIDSwift/PublicHeader.h>
 
+//#import <PaygisDIDSwift/PayegisDID.h>
 
